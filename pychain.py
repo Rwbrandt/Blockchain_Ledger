3315a8d6d1ffefc351d8f385ae6b38779530c7cc
Rwbrandt <brandt.rogerw@gmail.com>
2022-02-08 14:06:54 -0500

    Data Class creation

--- pychain.py
+++ pychain.py
@@ -36,9 +36,12 @@ import hashlib
 # Note that you’ll use this new `Record` class as the data type of your `record` attribute in the next section.
 
 
-# @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and `amount` attributes
-# YOUR CODE HERE
+@ dataclass
+class Record:
+    sender:str
+    receiver:str
+    amount:float
 
 
 ################################################################################
@@ -55,7 +58,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record: Record
 
     creator_id: int
     prev_hash: str = "0"
